d0d8b4ae49d7c44ea8a04b777127b4a7d0dcc4cb
jaspercloud <qq276726581@gmail.com>
2024-07-10 15:16:07 +0800

    commit

--- sdwan-tun/src/main/java/io/jaspercloud/sdwan/tun/linux/Iptables.java
+++ sdwan-tun/src/main/java/io/jaspercloud/sdwan/tun/linux/Iptables.java
@@ -1,9 +1,11 @@
 package io.jaspercloud.sdwan.tun.linux;
 
+import io.jaspercloud.sdwan.exception.ProcessException;
 import io.jaspercloud.sdwan.tun.ProcessUtil;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.util.FileCopyUtils;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.List;
 
 public final class Iptables {
@@ -12,17 +14,44 @@ public final class Iptables {
 
     }
 
-    public static void addFilterRule(String tunName, String ethName) throws IOException, InterruptedException {
+    public static void ipForward(String fromEth, String toEth) throws ProcessException {
+        try {
+            enableIpForward();
+            //filter
+            if (Iptables.queryFilterRule(fromEth, toEth)) {
+                Iptables.deleteFilterRule(fromEth, toEth);
+            }
+            Iptables.addFilterRule(fromEth, toEth);
+            //nat
+            if (Iptables.queryNatRule(toEth)) {
+                Iptables.deleteNatRule(toEth);
+            }
+            Iptables.addNatRule(toEth);
+        } catch (Exception e) {
+            throw new ProcessException(e.getMessage(), e);
+        }
+    }
+
+    private static void enableIpForward() {
+        File file = new File("/proc/sys/net/ipv4/ip_forward");
+        try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "utf-8"))) {
+            FileCopyUtils.copy("1", writer);
+        } catch (Throwable e) {
+            throw new ProcessException("enableIpForward failed", e);
+        }
+    }
+
+    private static void addFilterRule(String tunName, String ethName) throws IOException, InterruptedException {
         String cmd = String.format("iptables -t filter -A FORWARD -i %s -o %s -j ACCEPT", tunName, ethName);
         int code = ProcessUtil.exec(cmd);
     }
 
-    public static void addNatRule(String ethName) throws IOException, InterruptedException {
+    private static void addNatRule(String ethName) throws IOException, InterruptedException {
         String cmd = String.format("iptables -t nat -A POSTROUTING -o %s -j MASQUERADE", ethName);
         int code = ProcessUtil.exec(cmd);
     }
 
-    public static boolean queryFilterRule(String tunName, String ethName) throws IOException, InterruptedException {
+    private static boolean queryFilterRule(String tunName, String ethName) throws IOException, InterruptedException {
         String cmd = "iptables -t filter -L FORWARD -n -v";
         List<String> list = ProcessUtil.query(cmd);
         for (String line : list) {
@@ -33,7 +62,7 @@ public final class Iptables {
         return false;
     }
 
-    public static boolean queryNatRule(String ethName) throws IOException, InterruptedException {
+    private static boolean queryNatRule(String ethName) throws IOException, InterruptedException {
         String cmd = "iptables -t nat -L POSTROUTING -n -v";
         List<String> list = ProcessUtil.query(cmd);
         for (String line : list) {
@@ -44,12 +73,12 @@ public final class Iptables {
         return false;
     }
 
-    public static void deleteFilterRule(String tunName, String ethName) throws IOException, InterruptedException {
+    private static void deleteFilterRule(String tunName, String ethName) throws IOException, InterruptedException {
         String cmd = String.format("iptables -t filter -D FORWARD -i %s -o %s -j ACCEPT", tunName, ethName);
         int code = ProcessUtil.exec(cmd);
     }
 
-    public static void deleteNatRule(String ethName) throws IOException, InterruptedException {
+    private static void deleteNatRule(String ethName) throws IOException, InterruptedException {
         String cmd = String.format("iptables -t nat -D POSTROUTING -o %s -j MASQUERADE", ethName);
         int code = ProcessUtil.exec(cmd);
     }
