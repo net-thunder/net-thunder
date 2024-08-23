29161d09aaca1568ce565619e560035c37b5f594
jaspercloud <qq276726581@gmail.com>
2024-08-23 14:48:36 +0800

    commit windows

--- sdwan-node-bootstrap/src/main/java/io/jaspercloud/sdwan/platform/WindowsPlatformLauncher.java
+++ sdwan-node-bootstrap/src/main/java/io/jaspercloud/sdwan/platform/WindowsPlatformLauncher.java
@@ -1,21 +1,15 @@
 package io.jaspercloud.sdwan.platform;
 
 import ch.qos.logback.classic.Logger;
-import com.sun.jna.platform.win32.Winsvc;
 import io.jaspercloud.sdwan.node.ConfigSystem;
 import io.jaspercloud.sdwan.node.LoggerSystem;
 import io.jaspercloud.sdwan.node.SdWanNodeConfig;
 import io.jaspercloud.sdwan.node.TunSdWanNode;
-import io.jaspercloud.sdwan.support.*;
+import io.jaspercloud.sdwan.support.WinServiceManager;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Options;
-import org.apache.commons.lang3.StringUtils;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 public class WindowsPlatformLauncher {
 
@@ -37,87 +31,28 @@ public class WindowsPlatformLauncher {
             startService(cmd);
         } else if ("stop".equals(cmd.getOptionValue("t"))) {
             stopService(cmd);
+        } else if ("uninstall".equals(cmd.getOptionValue("t"))) {
+            uninstall(cmd);
         } else if ("service".equals(cmd.getOptionValue("t"))) {
             runService(cmd);
         }
     }
 
     private void mainProcess(CommandLine cmd) throws Exception {
-        String javaPath = getJavaPath();
-        List<String> argList = new ArrayList<>();
-        argList.add("-jar");
-        String executeJarPath = getExecuteJarPath();
-        String parentPath = new File(executeJarPath).getParent();
-        String configPath = new File(parentPath, "application.yaml").getAbsolutePath();
-        String logPath = new File(parentPath, "app.log").getAbsolutePath();
-        argList.add(executeJarPath);
-        argList.add("-t");
-        argList.add("start");
-        argList.add("-n");
-        SdWanNodeConfig config = new ConfigSystem().init(configPath);
-        String name = config.getTunName();
-        argList.add(name);
-        argList.add("-c");
-        argList.add(new File(configPath).getAbsolutePath());
-        argList.add("-log");
-        argList.add(new File(logPath).getAbsolutePath());
-        String args = StringUtils.join(argList, " ");
-        System.out.println(String.format("ShellExecuteW %s %s", javaPath, args));
-        WinShell.ShellExecuteW(javaPath, args, null, WinShell.SW_HIDE);
+        MainWindow mainWindow = new MainWindow();
+        mainWindow.setVisible(true);
     }
 
     private void startService(CommandLine cmd) throws Exception {
-        String logPath = cmd.getOptionValue("log");
-        String serviceName = cmd.getOptionValue("n");
-        Logger logger = new LoggerSystem().init(logPath);
-        logger.info("startService: {}", serviceName);
-        try (WinServiceManager scm = WinServiceManager.openExecute()) {
-            WinServiceManager.WinService winService = scm.openService(serviceName);
-            try {
-                if (null == winService) {
-                    createService(logger, cmd);
-                    winService = scm.openService(serviceName);
-                }
-                int status = queryServiceStatus(serviceName);
-                logger.info("service status={}", status);
-                if (Winsvc.SERVICE_RUNNING == status) {
-                    return;
-                }
-                winService.start();
-            } finally {
-                if (null != winService) {
-                    winService.close();
-                }
-            }
-        }
+        WinSvcUtil.startService(cmd);
     }
 
-    private int queryServiceStatus(String serviceName) {
-        try (WinServiceManager scm = WinServiceManager.openManager()) {
-            try (WinServiceManager.WinService winService = scm.openService(serviceName)) {
-                return winService.status();
-            }
-        }
+    private void stopService(CommandLine cmd) {
+        WinSvcUtil.stopService(cmd);
     }
 
-    private void stopService(CommandLine cmd) throws Exception {
-        String logPath = cmd.getOptionValue("log");
-        String serviceName = cmd.getOptionValue("n");
-        Logger logger = new LoggerSystem().init(logPath);
-        logger.info("stopService");
-        try (WinServiceManager scm = WinServiceManager.openExecute()) {
-            WinServiceManager.WinService winService = scm.openService(serviceName);
-            if (null == winService) {
-                return;
-            }
-            try {
-                winService.stop();
-            } finally {
-                if (null != winService) {
-                    winService.close();
-                }
-            }
-        }
+    private void uninstall(CommandLine cmd) {
+        WinSvcUtil.uninstall(cmd);
     }
 
     private void runService(CommandLine cmd) throws Exception {
@@ -149,40 +84,5 @@ public class WindowsPlatformLauncher {
         });
         logger.info("stopServiceCtrlDispatcher");
         System.exit(0);
-    }
-
-    private String getJavaPath() throws Exception {
-        String javaHome = System.getProperty("java.home");
-        String java = new File(new File(javaHome, "bin"), "java").getAbsolutePath();
-        return java;
-    }
-
-    private String getExecuteJarPath() throws Exception {
-        String userDir = System.getProperty("user.dir");
-        String path = new File(userDir, "sdwan-node-bootstrap.jar").getAbsolutePath();
-        return path;
-    }
-
-    private void createService(Logger logger, CommandLine cmd) throws Exception {
-        List<String> argList = new ArrayList<>();
-        argList.add(getJavaPath());
-        argList.add("-jar");
-        argList.add(getExecuteJarPath());
-        argList.add("-t");
-        argList.add("service");
-        argList.add("-n");
-        String name = cmd.getOptionValue("n");
-        argList.add(name);
-        String configPath = cmd.getOptionValue("c");
-        argList.add("-c");
-        argList.add(new File(configPath).getAbsolutePath());
-        String logPath = cmd.getOptionValue("log");
-        argList.add("-log");
-        argList.add(new File(logPath).getAbsolutePath());
-        String path = StringUtils.join(argList, " ");
-        logger.info("createService: {}", path);
-        try (WinServiceManager scm = WinServiceManager.openManager()) {
-            scm.createService(name, path);
-        }
     }
 }
