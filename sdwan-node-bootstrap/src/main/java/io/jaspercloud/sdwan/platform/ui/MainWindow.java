0f91aac3fa03d350b374b93e2a65ae8244da856e
jaspercloud <qq276726581@gmail.com>
2024-08-29 17:27:58 +0800

    commit

--- sdwan-node-bootstrap/src/main/java/io/jaspercloud/sdwan/platform/ui/MainWindow.java
+++ sdwan-node-bootstrap/src/main/java/io/jaspercloud/sdwan/platform/ui/MainWindow.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class MainWindow extends Application {
@@ -12,9 +13,11 @@ public class MainWindow extends Application {
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("ui/main.fxml"));
         Parent root = loader.load();
+        Image icon = new Image(getClass().getClassLoader().getResourceAsStream("ui/favicon.ico"));
+        primaryStage.getIcons().add(icon);
         MainWindowController controller = loader.getController();
         primaryStage.setOnCloseRequest(controller::handleWindowClose);
-        primaryStage.setTitle("SD-WAN Node");
+        primaryStage.setTitle("SD-WAN UI");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
