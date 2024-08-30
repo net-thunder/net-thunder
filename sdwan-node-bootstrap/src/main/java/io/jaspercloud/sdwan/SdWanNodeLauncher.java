48459d5de9a2f99f56835b04fedd57cb30600198
jaspercloud <qq276726581@gmail.com>
2024-08-30 17:12:03 +0800

    fix SdWanNodeLauncher

--- sdwan-node-bootstrap/src/main/java/io/jaspercloud/sdwan/SdWanNodeLauncher.java
+++ sdwan-node-bootstrap/src/main/java/io/jaspercloud/sdwan/SdWanNodeLauncher.java
@@ -7,6 +7,10 @@ import io.jaspercloud.sdwan.node.SdWanNodeConfig;
 import io.jaspercloud.sdwan.node.TunSdWanNode;
 import io.jaspercloud.sdwan.platform.WindowsPlatformLauncher;
 import io.netty.util.internal.PlatformDependent;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.concurrent.CountDownLatch;
@@ -15,15 +19,30 @@ import java.util.concurrent.CountDownLatch;
 public class SdWanNodeLauncher {
 
     public static void main(String[] args) throws Exception {
-        if (PlatformDependent.isWindows()) {
-            WindowsPlatformLauncher.startup(args);
+        Options options = new Options();
+        options.addOption("t", "type", true, "type");
+        options.addOption("a", "action", true, "action");
+        options.addOption("n", "name", true, "name");
+        options.addOption("c", "config", true, "config");
+        options.addOption("log", "logFile", true, "logFile");
+        options.addOption("debug", "debug", false, "debug");
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse(options, args);
+        if (cmd.hasOption("debug")) {
+            mainRun();
+        } else if (PlatformDependent.isWindows()) {
+            WindowsPlatformLauncher.startup(cmd);
         } else {
-            Logger logger = new LoggerSystem().initUserDir();
-            SdWanNodeConfig config = new ConfigSystem().initUserDir();
-            TunSdWanNode tunSdWanNode = new TunSdWanNode(config);
-            tunSdWanNode.start();
-            CountDownLatch latch = new CountDownLatch(1);
-            latch.await();
+            mainRun();
         }
+    }
+
+    private static void mainRun() throws Exception {
+        Logger logger = new LoggerSystem().initUserDir();
+        SdWanNodeConfig config = new ConfigSystem().initUserDir();
+        TunSdWanNode tunSdWanNode = new TunSdWanNode(config);
+        tunSdWanNode.start();
+        CountDownLatch latch = new CountDownLatch(1);
+        latch.await();
     }
 }
--- sdwan-node-bootstrap/src/main/java/io/jaspercloud/sdwan/platform/WindowsPlatformLauncher.java
+++ sdwan-node-bootstrap/src/main/java/io/jaspercloud/sdwan/platform/WindowsPlatformLauncher.java
@@ -5,21 +5,10 @@ import io.jaspercloud.sdwan.service.ManagerService;
 import io.jaspercloud.sdwan.service.TunnelService;
 import javafx.application.Application;
 import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.Options;
 
 public class WindowsPlatformLauncher {
 
-    public static void startup(String[] args) throws Exception {
-        Options options = new Options();
-        options.addOption("t", "type", true, "type");
-        options.addOption("a", "action", true, "action");
-        options.addOption("n", "name", true, "name");
-        options.addOption("c", "config", true, "config");
-        options.addOption("log", "logFile", true, "logFile");
-        CommandLineParser parser = new DefaultParser();
-        CommandLine cmd = parser.parse(options, args);
+    public static void startup(CommandLine cmd) throws Exception {
         new WindowsPlatformLauncher().run(cmd);
     }
 
