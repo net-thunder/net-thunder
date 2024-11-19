b81962ab89b939ea9a1e75b74d9de51820184d82
jaspercloud <qq276726581@gmail.com>
2024-11-19 15:40:16 +0800

    fix ChannelSpace

--- sdwan-core/src/main/java/io/jaspercloud/sdwan/tranport/ChannelSpace.java
+++ sdwan-core/src/main/java/io/jaspercloud/sdwan/tranport/ChannelSpace.java
@@ -1,6 +1,8 @@
 package io.jaspercloud.sdwan.tranport;
 
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -15,6 +17,12 @@ public class ChannelSpace {
     }
 
     public void addChannel(String vip, Channel channel) {
+        channel.closeFuture().addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture channelFuture) throws Exception {
+                channelMap.remove(vip);
+            }
+        });
         channelMap.put(vip, channel);
     }
 
