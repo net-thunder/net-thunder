08b5398b8c3e997f10bc95f02ec1ecb44e19b606
jaspercloud <qq276726581@gmail.com>
2024-11-20 12:17:27 +0800

    fix controller

--- sdwan-controller/src/main/java/io/jaspercloud/sdwan/server/controller/common/ValidGroup.java
+++ sdwan-controller/src/main/java/io/jaspercloud/sdwan/server/controller/common/ValidGroup.java
@@ -4,7 +4,7 @@ import cn.hutool.core.lang.RegexPool;
 
 public interface ValidGroup {
 
-    String NAME = "^\\w{4,16}$";
+    String NAME = "^[\\u4e00-\\u9fa5a-zA-Z0-9]{4,16}$";
     String CODE = "^\\w{4,10}";
     String UNAME_PWD = "^\\w{6,18}";
     String MAC_ADDRESS = RegexPool.MAC_ADDRESS;
