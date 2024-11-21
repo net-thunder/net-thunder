693a7cf27b3ee054353d35c476d254351fb3deb3
jaspercloud <qq276726581@gmail.com>
2024-11-21 14:05:20 +0800

    update new ui

--- sdwan-common/src/main/java/io/jaspercloud/sdwan/util/PlatformUtil.java
+++ sdwan-common/src/main/java/io/jaspercloud/sdwan/util/PlatformUtil.java
@@ -1,7 +1,19 @@
 package io.jaspercloud.sdwan.util;
 
+import io.netty.util.internal.PlatformDependent;
+
 public class PlatformUtil {
 
     public static final String WINDOWS = "windows";
     public static final String MACOS = "macos";
+
+    public static String normalizedOs() {
+        if (PlatformDependent.isWindows()) {
+            return WINDOWS;
+        } else if (PlatformDependent.isOsx()) {
+            return MACOS;
+        } else {
+            return PlatformDependent.normalizedOs();
+        }
+    }
 }
