34d615407dcfe5decb2edfe6999a70fde9bdebe1
jaspercloud <qq276726581@gmail.com>
2024-11-30 10:39:08 +0800

    update routeRule

--- sdwan-tun/src/main/java/io/jaspercloud/sdwan/route/rule/RouteRulePredicateProcessor.java
+++ sdwan-tun/src/main/java/io/jaspercloud/sdwan/route/rule/RouteRulePredicateProcessor.java
@@ -2,36 +2,31 @@ package io.jaspercloud.sdwan.route.rule;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class RouteRulePredicateProcessor implements RouteRulePredicate {
 
     private List<RouteRuleChain> rejectList;
-    private List<RouteRuleChain> allowList;
+    private List<RouteRuleChain> predicateList;
 
     public RouteRulePredicateProcessor() {
         this(Collections.emptyList());
     }
 
     public RouteRulePredicateProcessor(List<RouteRuleChain> predicateList) {
-        this.rejectList = predicateList.stream()
-                .filter(e -> RouteRuleStrategyEnum.Reject.equals(e.getStrategy()))
-                .collect(Collectors.toList());
-        this.allowList = predicateList.stream()
-                .filter(e -> RouteRuleStrategyEnum.Allow.equals(e.getStrategy()))
-                .collect(Collectors.toList());
+        this.predicateList = predicateList;
     }
 
     @Override
     public boolean test(String ip) {
-        for (RouteRuleChain predicate : rejectList) {
-            if (false == predicate.test(ip)) {
-                return false;
-            }
-        }
-        for (RouteRuleChain predicate : allowList) {
-            if (true == predicate.test(ip)) {
-                return true;
+        for (RouteRuleChain predicate : predicateList) {
+            if (predicate.test(ip)) {
+                if (RouteRuleStrategyEnum.Allow.equals(predicate.getStrategy())) {
+                    return true;
+                } else if (RouteRuleStrategyEnum.Reject.equals(predicate.getStrategy())) {
+                    return false;
+                } else {
+                    throw new UnsupportedOperationException();
+                }
             }
         }
         return false;
