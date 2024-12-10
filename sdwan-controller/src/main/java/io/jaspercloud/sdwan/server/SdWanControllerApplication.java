a7f994a57baa29a1332be61578b03e376ea3b143
jaspercloud <qq276726581@gmail.com>
2024-12-10 23:12:01 +0800

    update derby

--- sdwan-controller/src/main/java/io/jaspercloud/sdwan/server/SdWanControllerApplication.java
+++ sdwan-controller/src/main/java/io/jaspercloud/sdwan/server/SdWanControllerApplication.java
@@ -1,7 +1,11 @@
 package io.jaspercloud.sdwan.server;
 
+import org.apache.commons.cli.Options;
+import org.apache.derby.tools.ij;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
 
 /**
  * @author jasper
@@ -11,6 +15,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SdWanControllerApplication {
 
     public static void main(String[] args) throws Exception {
+        Options options = new Options();
+        options.addOption("derby-shell", "derby-shell", false, "derby shell");
+        if (options.hasOption("derby-shell")) {
+            String userDir = System.getProperty("user.dir");
+            File file = new File(userDir, "derby");
+            System.out.println(String.format("connect 'jdbc:derby:%s;create=true';", file.getAbsolutePath()));
+            ij.main(new String[]{});
+            return;
+        }
         SpringApplication.run(SdWanControllerApplication.class, args);
     }
 }
--- sdwan-controller/src/main/java/io/jaspercloud/sdwan/server/config/DerbyConfig.java
+++ sdwan-controller/src/main/java/io/jaspercloud/sdwan/server/config/DerbyConfig.java
@@ -3,30 +3,16 @@ package io.jaspercloud.sdwan.server.config;
 import com.alibaba.druid.pool.DruidDataSource;
 import com.baomidou.mybatisplus.autoconfigure.MybatisPlusAutoConfiguration;
 import com.baomidou.mybatisplus.core.injector.ISqlInjector;
-import io.jaspercloud.sdwan.exception.ProcessException;
 import io.jaspercloud.sdwan.server.support.derby.DerbyISqlInjector;
 import io.jaspercloud.sdwan.server.support.derby.LimitInterceptor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.util.StreamUtils;
 
 import javax.sql.DataSource;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.nio.charset.Charset;
-import java.sql.Connection;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
 
 @Slf4j
 @AutoConfigureBefore(MybatisPlusAutoConfiguration.class)
@@ -35,24 +21,12 @@ import java.util.List;
 public class DerbyConfig {
 
     @Bean
-    public ApplicationListener<ApplicationReadyEvent> environmentPreparedEvent() {
-        return event -> {
-            System.out.println();
-        };
-    }
-
-    @Bean
-    public DataSource dataSource(@Value("${spring.datasource.path}") String path,
-                                 Environment env) throws Exception {
+    public DataSource dataSource(@Value("${spring.datasource.path}") String path) {
         String driver = "org.apache.derby.jdbc.EmbeddedDriver";
         String jdbcUrl = String.format("jdbc:derby:%s;create=true", path);
         DruidDataSource dataSource = new DruidDataSource();
         dataSource.setDriverClassName(driver);
         dataSource.setUrl(jdbcUrl);
-        String initSql = env.getProperty("spring.datasource.init-sql");
-        if (null != initSql) {
-            executeSql(dataSource, initSql);
-        }
         return dataSource;
     }
 
@@ -64,34 +38,5 @@ public class DerbyConfig {
     @Bean
     public LimitInterceptor limitInterceptor() {
         return new LimitInterceptor();
-    }
-
-    private void executeSql(DataSource dataSource, String path) throws Exception {
-        File file = new File(path);
-        if (!file.exists()) {
-            throw new ProcessException("sqlFile not found");
-        }
-        List<String> sqlList = new ArrayList<>();
-        try (InputStream stream = new FileInputStream(file)) {
-            String text = StreamUtils.copyToString(stream, Charset.forName("utf-8"));
-            String[] split = text.split(";");
-            for (String sp : split) {
-                String sql = sp.replaceAll("--.*", "").trim();
-                if (StringUtils.isNotEmpty(sql)) {
-                    sqlList.add(sql);
-                }
-            }
-        }
-        try (Connection connection = dataSource.getConnection()) {
-            try (Statement statement = connection.createStatement()) {
-                for (String sql : sqlList) {
-                    try {
-                        statement.execute(sql);
-                    } catch (Exception e) {
-                        log.error(e.getMessage(), e);
-                    }
-                }
-            }
-        }
     }
 }
