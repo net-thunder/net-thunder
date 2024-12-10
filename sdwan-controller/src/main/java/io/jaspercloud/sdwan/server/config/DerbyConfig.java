773566e1754f0e275222ca927e02ca11df18f088
jaspercloud <qq276726581@gmail.com>
2024-12-10 13:32:46 +0800

    update sql

--- sdwan-controller/src/main/java/io/jaspercloud/sdwan/server/config/DerbyConfig.java
+++ sdwan-controller/src/main/java/io/jaspercloud/sdwan/server/config/DerbyConfig.java
@@ -3,6 +3,7 @@ package io.jaspercloud.sdwan.server.config;
 import com.alibaba.druid.pool.DruidDataSource;
 import com.baomidou.mybatisplus.autoconfigure.MybatisPlusAutoConfiguration;
 import com.baomidou.mybatisplus.core.injector.ISqlInjector;
+import io.jaspercloud.sdwan.exception.ProcessException;
 import io.jaspercloud.sdwan.server.support.derby.DerbyISqlInjector;
 import io.jaspercloud.sdwan.server.support.derby.LimitInterceptor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,12 +11,16 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.util.StreamUtils;
 
 import javax.sql.DataSource;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.sql.Connection;
@@ -30,13 +35,24 @@ import java.util.List;
 public class DerbyConfig {
 
     @Bean
-    public DataSource dataSource(@Value("${spring.datasource.path}") String path) throws Exception {
+    public ApplicationListener<ApplicationReadyEvent> environmentPreparedEvent() {
+        return event -> {
+            System.out.println();
+        };
+    }
+
+    @Bean
+    public DataSource dataSource(@Value("${spring.datasource.path}") String path,
+                                 Environment env) throws Exception {
         String driver = "org.apache.derby.jdbc.EmbeddedDriver";
         String jdbcUrl = String.format("jdbc:derby:%s;create=true", path);
         DruidDataSource dataSource = new DruidDataSource();
         dataSource.setDriverClassName(driver);
         dataSource.setUrl(jdbcUrl);
-        initDerby(dataSource, path);
+        String initSql = env.getProperty("spring.datasource.init-sql");
+        if (null != initSql) {
+            executeSql(dataSource, initSql);
+        }
         return dataSource;
     }
 
@@ -50,15 +66,13 @@ public class DerbyConfig {
         return new LimitInterceptor();
     }
 
-    private void initDerby(DataSource dataSource, String path) throws Exception {
-        File dbPathFile = new File(path);
-        boolean exists = dbPathFile.exists();
-        if (exists) {
-            return;
+    private void executeSql(DataSource dataSource, String path) throws Exception {
+        File file = new File(path);
+        if (!file.exists()) {
+            throw new ProcessException("sqlFile not found");
         }
         List<String> sqlList = new ArrayList<>();
-        try (InputStream stream = Thread.currentThread()
-                .getContextClassLoader().getResourceAsStream("META-INF/init-schema.sql")) {
+        try (InputStream stream = new FileInputStream(file)) {
             String text = StreamUtils.copyToString(stream, Charset.forName("utf-8"));
             String[] split = text.split(";");
             for (String sp : split) {
