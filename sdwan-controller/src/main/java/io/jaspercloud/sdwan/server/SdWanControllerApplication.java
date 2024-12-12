0008f0d416e02fe60ce50a4fed0d1c9174bdb436
jaspercloud <qq276726581@gmail.com>
2024-12-12 17:28:59 +0800

    update config

--- sdwan-controller/src/main/java/io/jaspercloud/sdwan/server/SdWanControllerApplication.java
+++ sdwan-controller/src/main/java/io/jaspercloud/sdwan/server/SdWanControllerApplication.java
@@ -1,5 +1,8 @@
 package io.jaspercloud.sdwan.server;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Options;
 import org.apache.derby.tools.ij;
 import org.springframework.boot.SpringApplication;
@@ -17,7 +20,9 @@ public class SdWanControllerApplication {
     public static void main(String[] args) throws Exception {
         Options options = new Options();
         options.addOption("derby-shell", "derby-shell", false, "derby shell");
-        if (options.hasOption("derby-shell")) {
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse(options, args);
+        if (cmd.hasOption("derby-shell")) {
             String userDir = System.getProperty("user.dir");
             File file = new File(userDir, "derby");
             System.out.println(String.format("connect 'jdbc:derby:%s;create=true';", file.getAbsolutePath()));
